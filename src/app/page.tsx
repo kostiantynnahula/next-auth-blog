d513dac53b447865911c1d0945fed310389a44e7
Kostiantyn Nahula <kostyannagula@gmail.com>
2023-08-07 07:49:31 +0300

    Implemented about and contact components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "public/hero.png";
+import Button from '@/components/button/Button'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The future of AI in the next few years</h1>
+        <p>Turning your Idea into Reality. We bring together the teams from the global tech industry.</p>
+        <Button url="/portfolio" text="See our works"/>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='main photo' className={styles.img}/>
+      </div>
+    </main>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,15 @@
+import styles from './page.module.css';
+import Image from 'next/image';
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.container}>
       <div>2023 MyApp. All right reserved</div>
+      <div className={styles.social}>
+        <Image src='/inst.png' width={15} height={15} className={styles.icon} alt={'instagram'}/>
+        <Image src='/twitter.png' width={15} height={15} className={styles.icon} alt={'twintter'}/>
+        <Image src='/yt.png' width={15} height={15} className={styles.icon} alt={'youtube'}/>
+      </div>
     </footer>
   )
 }
